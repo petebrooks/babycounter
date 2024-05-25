cd5474346e87e55a3e15104d35d620b987c1dfe6
petelouise <programmetress@petelouise.beauty>
2024-05-25 17:38:16 -0500

    Refactored code to sort baby counts in descending order.

--- babycounter/main.py
+++ babycounter/main.py
@@ -4,15 +4,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# Replace 'YOUR_GENIUS_API_KEY' with your actual Genius API key
-genius = lyricsgenius.Genius(
-    os.getenv("GENIUS_API_KEY")
-)  # , verbose=False, remove_section_headers=True)
+genius = lyricsgenius.Genius(os.getenv("GENIUS_API_KEY"))
+
 
 # Fetch the top 100 Janet Jackson songs
 def fetch_top_songs(artist_name, max_songs=100):
     artist = genius.search_artist(artist_name, max_songs=max_songs)
     return [song.title for song in artist.songs]
+
 
 songs = fetch_top_songs("Janet Jackson")
 
@@ -29,7 +28,9 @@ def count_babies(song_title):
 if __name__ == "__main__":
     # Count "baby" in each song
     baby_counts = {song: count_babies(song) for song in songs}
-    baby_counts_sorted = sorted(baby_counts.items(), key=lambda item: item[1], reverse=True)
+    baby_counts_sorted = sorted(
+        baby_counts.items(), key=lambda item: item[1], reverse=True
+    )
 
     for song, count in baby_counts_sorted:
         print(f"{song}: {count} times")
